be7b5d88567e885f3596ec3e60a7a8c30f9522a0
Gaijin81 <stephane.gagnant@sncf.fr>
2022-04-07 12:20:58 +0200

    add checkPassword

--- back-end/src/users/users.routes.js
+++ back-end/src/users/users.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const userController = require('./controllers/users.controller')
-
+const userMiddleware = require('./middlewares/users.middleware')
 const router = express.Router()
 
 router.get('/', userController.readUsers)
 router.put('/:id', userController.updateUsers)
-router.post('/', userController.createUsers)
+router.post('/',[userMiddleware.checkPassword, userController.createUsers] )
 router.delete('/:id', userController.deleteUsers)
+
 module.exports = router
