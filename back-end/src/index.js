a7a88018f19053a04061868e216b5bdfc03317f9
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-04-15 15:41:19 +0200

    port 5001

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,16 +3,14 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 
+const corsOptions = {
+  origin: '*',
+}
 //******* MIDDLEWARES *********//
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
-  next();
-});
+app.use(cors(corsOptions));
+
+
 
 const port = process.env.PORT
 
