70526503f5cab91b76b98b3eec3554b67fbddcd8
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-04-20 16:08:04 +0200

    post login

--- front-end/src/components/auth/Login.jsx
+++ front-end/src/components/auth/Login.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useRef, useState } from 'react';
 
 const Login = () => {
+    const login = useRef();
+    const password = useRef();
+    const [error, setError]= useState()
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:5001/users/login', { login: login.current.value, password: password.current.value })
+            .then((res) => {
+                alert('Vous etes connecté')
+            }).catch((error) => {
+                setError(error.response.data.error) // reponse de l'API
+            });
+    }
+
     return (
         <div className="login">
             <video id="background-video" autoPlay loop muted>
-
                 <source src="./images/production ID_4789847.mp4" type="video/mp4" />
-
             </video>
             <div className="card-login">
                 <img src='./images/logo.png' alt='logo' />
-                <form>
-                    <input type="text" placeholder='Numéro de CP*' />
-                    <input type="password" placeholder='Mot de passe*' />
+                <form onSubmit={e => handleLogin(e)}>
+                    <input type="text" placeholder='Numéro de CP*' ref={login} />
+                    <input type="password" placeholder='Mot de passe*' ref={password} />
                     <p>Mot de passe oublié ?</p>
-                    <button className='btn-login' type="submit">SE CONNECTER</button>
+                    <button type="submit" className='btn-login'>SE CONNECTER</button>
+                    <h3>{error}</h3>
                     <span> ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ OU ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</span>
                     <button className='btn-register'>S'INSCRIRE</button>
                 </form>
