551271dcc6b92a760f96feff54c4050861c8311a
Gaijin81 <stephane.gagnant@sncf.fr>
2022-04-20 16:05:31 +0200

    Add component Auth with optmisation Scss

--- front-end/src/components/auth/Login.jsx
+++ front-end/src/components/auth/Login.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div className="login">
-            <video id="background-video" autoPlay loop muted>
+        <div className="auth">
+            <div className="login">
+                <video id="background-video" autoPlay loop muted>
 
-                <source src="./images/production ID_4789847.mp4" type="video/mp4" />
+                    <source src="./images/production ID_4789847.mp4" type="video/mp4" />
 
-            </video>
-            <div className="card-login">
-                <img src='./images/logo.png' alt='logo' />
-                <form>
-                    <input type="text" placeholder='Numéro de CP*' />
-                    <input type="password" placeholder='Mot de passe*' />
-                    <p>Mot de passe oublié ?</p>
-                    <button className='btn-login' type="submit">SE CONNECTER</button>
-                    <span> ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ OU ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</span>
-                    <button className='btn-register'>S'INSCRIRE</button>
-                </form>
+                </video>
+                <div className="card-auth">                   
+                    <form>
+                    <img src='./images/logo.png' alt='logo' />
+                        <input type="text" placeholder='Numéro de CP*' />
+                        <input type="password" placeholder='Mot de passe*' />
+                        <p>Mot de passe oublié ?</p>
+                        <button className="btn-login" type="submit">SE CONNECTER</button>
+                        <p className="ligne_horizontal">&nbsp; OU &nbsp;</p>
+                        <button className="btn">S'INSCRIRE</button>
+                    </form>
+                </div>
             </div>
-
 
 
         </div>
