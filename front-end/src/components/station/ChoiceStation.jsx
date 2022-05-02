a0183571561347e0ebc4ef8caaee3b76cca99f09
frederic andolina <fredericandolina@GearS3-82E9.localdomain>
2022-05-02 14:46:34 +0200

    add axios and useEffect

--- front-end/src/components/station/ChoiceStation.jsx
+++ front-end/src/components/station/ChoiceStation.jsx
@@ -1,38 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
 
 
 const ChoiceStation = () => {
+    const [locality,setLocality] = useState({})
+    
+    useEffect(() => {
+        axios
+          .get("http://localhost:5001/station")
+          .then((response) => setCity(response.data[0]));
+      }, []);
     return (
         <div className="choice-station">
             <h1>CHOIX DE LA VILLE</h1>
             <div className="container-card">
                 <div className="card-station">
-                    <h3>PARIS</h3>
+                    <h3>{locality.city}</h3>
                 </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
-                <div className="card-station">
-                    <h3>PARIS</h3>
-                </div>
+                
 
             </div>
 
@@ -42,3 +30,17 @@ const ChoiceStation = () => {
 };
 
 export default ChoiceStation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
