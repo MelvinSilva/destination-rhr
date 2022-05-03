61805adb385524a47771e6ef4006cc83073478aa
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-05-03 23:21:57 +0200

    design accomodation in progress

--- front-end/src/components/tab/Accomodation.jsx
+++ front-end/src/components/tab/Accomodation.jsx
@@ -8,7 +8,7 @@ const Accomodation = () => {
     useEffect(() => {
         axios
             .get("http://localhost:5001/accomodations")
-            .then((response) => setDataAccomodation(response.data[12]));
+            .then((response) => setDataAccomodation(response.data[22]));
     }, []);
 
     return (
@@ -18,20 +18,16 @@ const Accomodation = () => {
                     <h2>Maps</h2>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21132.28412833883!2d2.637735875122883!3d48.54213257248209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e5fa64b0617d79%3A0xc7b890eb02e993a!2s%C3%89glise%20Saint-Aspais%20de%20Melun!5e0!3m2!1sfr!2sfr!4v1651498046192!5m2!1sfr!2sfr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div className="change-services">
-                    <button className="btn">Mettre à jour les services</button>
-                    <button className="btn btn--red">Supprimer la fiche (admin)</button>
-                </div>
-
             </div>
             <div className="container-center">
                 <h2>{dataAccomodation.accomodation}</h2>
                 <div className="info-accomodation">
-                    <p><span>🏡 Adresse :</span> {dataAccomodation.adress}</p>
-                    <p><span>📬 Mail :</span> {dataAccomodation.email}</p>
-                    <p><span>📱 Numéro :</span> {dataAccomodation.phone_number}</p>
-                    {dataAccomodation.distance_station && <p><span>📍 Distance gare :</span></p>}
-                    {dataAccomodation.reception && <p><span>🕥 Horaire accueil :</span></p>}
+                    {dataAccomodation.adress && <p><span>🏡 Adresse :</span> {dataAccomodation.adress}, {dataAccomodation.postal_code}</p>}
+                    {dataAccomodation.email && <p><span>📬 Mail : </span> {dataAccomodation.email}</p>}
+                    {dataAccomodation.phone_number && <p><span>📱 Numéro :</span> {dataAccomodation.phone_number}</p>}
+                    {dataAccomodation.distance_station && <p><span>📍 Distance gare : </span>{dataAccomodation.distance_station}</p>}
+                    {dataAccomodation.reception && <p><span>🕥 Horaire accueil : </span>{dataAccomodation.reception}</p>}
+                    <img src="https://www.cheminots.net/uploads/monthly_2016_09/image.jpeg.33ce4e56e28f1c14de4147a06b3acdb4.jpeg" />
                 </div>
             </div>
             <div className="container-right">
@@ -47,6 +43,10 @@ const Accomodation = () => {
                     {dataAccomodation.terrace === 1 && <p>🪟 Terrasse</p>}
                     {dataAccomodation.board_games === 1 && <p>🎲 Jeux de sociétés</p>}
                     {dataAccomodation.website && <p>💻 <a href={dataAccomodation.website}>Site internet</a></p>}
+                    <div className="update-services">
+                        <button className="btn">Mettre à jour les informations</button>
+                        <button className="btn btn--red">Supprimer la fiche</button>
+                    </div>
 
                 </div>
             </div>
