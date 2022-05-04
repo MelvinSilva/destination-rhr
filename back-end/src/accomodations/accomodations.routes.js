7b6edaca1e4449c579ca725d6929e081c712ace5
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-05-04 18:23:03 +0200

    update back

--- back-end/src/accomodations/accomodations.routes.js
+++ back-end/src/accomodations/accomodations.routes.js
@@ -4,6 +4,7 @@ const accomodationController = require('./controllers/accomodation.controller')
 const router = express.Router()
 
 router.get('/', accomodationController.listAccomodations)
+router.get('/:id_station', accomodationController.listAccomodationStation)
 router.put('/:id', accomodationController.updateAccomodation)
 router.delete('/:id', accomodationController.deleteAccomodation)
 
