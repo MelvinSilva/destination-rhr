b60bb928e3907cc24d77eaf5873267f258e09f1a
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-05-18 15:57:40 +0200

    back ok

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const app = express();
 
 const corsOptions = {
@@ -10,6 +11,8 @@ const corsOptions = {
 //******* MIDDLEWARES *********//
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use(cookieParser());
+
 
 
 const port = process.env.PORT
