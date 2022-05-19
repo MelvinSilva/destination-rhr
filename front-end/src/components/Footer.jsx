4c737fb7fe01c13dc754d43ce86736be73f5c5a7
Gaijin81 <stephane.gagnant@sncf.fr>
2022-05-19 19:20:23 +0200

    add footer logos

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -4,6 +4,10 @@ import { RiAdminFill } from 'react-icons/ri';
 function Footer() {
   return (
     <div className="footer">
+      <div className="logo-footer-left">
+        <a href="https://graou.info/apropos"><img className="logo" src="/images/footer/graou.jpg" alt="logo-graou" /></a>
+        <a href="https://www.orfea.fr/fr"><img className="logo" src="/images/footer/orfea.jpg" alt="logo-orfea" /></a>
+      </div>
       <div className="infos-footer">
         <p>Destination RHR by Jean-Mi, Fred, Steph & Melvin @ Wild Code School</p>
         <a href="/administration">
@@ -13,6 +17,10 @@ function Footer() {
             espace administration
           </p>
         </a>
+      </div>
+      <div className="logo-footer-right">
+        <a href="https://www.sncf-connect.com/"><img className="logo" src="/images/footer/connect.jpg" alt="logo-connect" /></a>
+        <a href="https://wit.sncf.fr/"><img className="logo" src="/images/footer/wit.jpg" alt="logo-wit" /></a>
       </div>
     </div>
   );
