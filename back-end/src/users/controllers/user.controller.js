f0862860d51fd19c8d9f4c63966fa9e90c37f003
JeanMichel999 <93377201+JeanMichel999@users.noreply.github.com>
2022-06-14 09:33:55 +0200

    maj

--- back-end/src/users/controllers/user.controller.js
+++ back-end/src/users/controllers/user.controller.js
@@ -17,7 +17,7 @@ class UserController {
         try {
             const user = req.body  // récupère tout et tous les champs sont modifiables
             const result = await userModel.update(user, req.params.id)
-            res.status(200).send(result)
+            res.status(204).send(result)
         }
         catch (error) {
             res.status(500).send({ error: error.message })
